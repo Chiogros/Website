aba80aaa8a9430254ea416854d4427f6daa3cc2f
Chiogros <alexandre.bouijoux@gmail.com>
2021-06-17 11:10:58 +0200

    Project model and migration

---
+++ database/migrations/2021_06_17_085204_create_projects_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('content');
+            $table->string('image')->nullable();
+            $table->date('begin');
+            $table->date('end');
+            $table->string('skills');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
