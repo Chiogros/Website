5e63b26d641b802556fe211cfaaba256be066b8c
Chiogros <alexandre.bouijoux@gmail.com>
2022-01-13 19:31:48 +0100

    Homepage presentation

--- view/homepage.php
+++ view/homepage.php
@@ -15,6 +15,17 @@
 	
 	<section>
 		<h2>Hey !</h2>
+		<p>
+Here Alexandre, in France.<br>
+<br>
+I'm studying IT for Networks & Systems Administration and Security.<br>
+Full time Linux user since birth, I like to learn about technical aspects of a topic or about a concept.<br>
+Following F.O.S.S. spirit, I'm sharing my Open-Source projects throught my GitHub profile.<br>
+<br>
+Apart from brainfuck activities, I love being outside among trees or mountains while trekking, orienteering or mountainbiking.<br>
+<br>
+Currently studying: Blockchain, Cybersecurity<br>
+    </p>
 	</section>
 	
 	<footer>
