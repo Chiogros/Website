0946d35e220a75390b221a6801e0377d13df9533
Chiogros <alexandre.bouijoux@gmail.com>
2022-01-14 10:52:07 +0100

    Links in description

--- view/homepage.php
+++ view/homepage.php
@@ -21,7 +21,7 @@ Here Alexandre, in France.<br>
 <br>
 I'm studying IT for Networks & Systems Administration and Security.<br>
 Full time Linux user since birth, I like to learn about technical aspects of a topic or about a concept.<br>
-Following F.O.S.S. spirit, I'm sharing my Open-Source projects throught my GitHub profile.<br>
+Following <a href="https://itsfoss.com/what-is-foss/">F.O.S.S.</a> spirit, I'm sharing my Open-Source projects throught my <a href="https://github.com/Chiogros">GitHub profile</a>.<br>
 <br>
 Apart from brainfuck activities, I love being outside among trees or mountains while trekking, orienteering or mountainbiking.<br>
 <br>
