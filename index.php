cffc2bf98c42cb70d50f6522c11745df335d92f1
Alexandre <alexandre.bouijoux@gmail.com>
2022-04-11 12:05:15 +0200

    New website HTML

--- index.php
+++ index.php
@@ -1,4 +1,32 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="style.css">
+	<link rel="icon" href="data:,">
+	<title>Alexandre Bouijoux</title>
+</head>
+<body>
+	<header>
+	</header>
+	
+	<section>
+		<img id="logo" src="assets/logo.png" alt="Profile picture"/>
+		<h2>Chiogros</h2>
+		<p>Sysadmin student</p>
 
-header("Location: ./view/homepage.php");
-die();
+		<div id="networks">
+			<a href="https://fosstodon.org/web/@Chiogros" target="_blank"><img id="mastodon" src="assets/mastodon.svg" alt="Mastodon logo"/></a>
+			<a href="https://github.com/Chiogros" target="_blank"><img id="github" src="assets/github.svg" alt="GitHub logo"/></a>
+			<a href="https://www.root-me.org/Chiogros" target="_blank"><img id="rootme" src="assets/rootme.svg" alt="RootMe logo"/></a>
+			<a href="mailto:alexandre.bouijoux@gmail.com" target="_blank"><p id="email">@</p></a>
+		</div>
+	</section>
+	
+	<footer>
+		<p>Made with <a href="https://www.vim.org/">Vim</a>.</p>
+	</footer>
+</body>
+</html>
+
+
