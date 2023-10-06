f77f8caa3b9c4f82bdff3fef006cdf8c0ebee7a3
Alexandre Bouijoux <alexandre@bouijoux.fr>
2023-10-06 10:16:21 +0200

    Revert "Merge pull request #3 from RemiGonin/remi.gonin/perso/alexandre-oui-bouijour"
    This reverts commit 50a7f2480881a61d01bfe93ee65f628efdf70d11, reversing
    changes made to 7608171e5912a945b26fd6c7cafa13b7022cf395.

--- scripts/bouijour.js
+++
@@ -1,9 +0,0 @@
-const rainbow = ["red", "orange", "yellow", "green", "cyan", "blue", "purple", "pink"];
-
-window.setInterval(function () {
-    const color = rainbow.shift();
-    document.documentElement.style.setProperty("--highlight-color", color);
-    rainbow.push(color);
-}, 100);
-
-// C'est nul sans les types, vive typescript
